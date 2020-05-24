864fe2fabc8042fd8d716c7ee27153f1eab1964b
Mossaab Tazi <mosstazi@gmail.com>
2020-05-24 04:30:10 +0100

    commit

---
+++ AssembliesAndNamespaces/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Xml.Linq;
+
+namespace AssembliesAndNamespaces
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            var doc = new XDocument();
+
+        /*  string s1 = "Hello";
+            String s2 = "World";    */
+        }
+    }
+}
